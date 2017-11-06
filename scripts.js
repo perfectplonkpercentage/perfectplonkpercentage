705ffda5748af49d73be5c9a5602b3f1ad68e76d
maartjegelsink <maartje.weenink@student.ru.nl>
2017-11-06 20:46:16 +0100

    working multiply base code

--- scripts.js
+++ scripts.js
@@ -1,35 +1,16 @@
 $(document).ready(function() {
     // all custom jQuery will go here
 
-    function getTotal()
+    $(document).ready(function(){
+        $('#a1').keyup(calculate);
+        $('#a2').keyup(calculate);
+    });
+    function calculate(e)
     {
-        //Here we get the total price by calling our function
-        //Each function returns a number so by calling them we add the values they return together
-        var cakePrice = getCakeSizePrice() + getFillingPrice() +
-                              candlesPrice() + insciptionPrice();
-
-        //display the result
-        document.getElementById('totalPrice').innerHTML =
-                                          "Total Price For Cake $"+cakePrice;
-
+        $('#a3').val($('#a1').val() * $('#a2').val());
     }
 
 
-
-    function calculate() {
-  		var myBox1 = document.getElementById('box1').value;
-  		var myBox2 = document.getElementById('box2').value;
-  		var result = document.getElementById('result');
-  		var myResult = myBox1 * myBox2;
-  		result.value = myResult;
-  	}
-
-    function calc(){
-       var textValue1 = document.getElementById('input1').value;
-       var textValue2 = document.getElementById('input2').value;
-
-       document.getElementById('output').value = textValue1 * textValue2;
-     }
 
 var myName = "Maartje";
 
