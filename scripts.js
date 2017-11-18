3f3514f4465b3d37261aaaccca0f8a3f783bde41
maartjegelsink <maartje.weenink@student.ru.nl>
2017-11-18 15:55:49 +0000

    calculation 2 and 3 test

--- scripts.js
+++ scripts.js
@@ -1,24 +1,12 @@
 $(document).ready(function() {
     // all custom jQuery will go here
 
-    $(document).ready(function(){
-        $('#a1').keyup(calculate);
-        $('#a2').keyup(calculate);
-    });
-
-
-    function calculate(e)
-    {
-        $('#a3').val($('#a1').val() * $('#a2').val());
-    }
-
+//calculations - row 1
     $(document).ready(function(){
         $('#volume1').keyup(calculate);
         $('#price1').keyup(calculate);
         $('#percentage1').keyup(calculate);
-        $('#percentage1').keyup(calculate);
     });
-
 
     function calculate(e)
     {
@@ -29,21 +17,43 @@ $(document).ready(function() {
         var result1 = price1 / (volume1 * percentage1)
 
         $("#ppu1").val(result1)
-
-        //$('#ppu1').val($('#volume1').val() * $('#percentage1').val());
-        //$('#ppu1').val($('#price1').val() / (($('#volume1').val() * $('#percentage1').val())));
-        //var vp1 = $('#volume1').val() * $('#percentage1').val();
-        //$('#ppu1').val($('#price1').val() / $(vp1).val());
-
     }
 
+//calculations - row 2
+        $(document).ready(function(){
+            $('#volume2').keyup(calculate);
+            $('#price2').keyup(calculate);
+            $('#percentage2').keyup(calculate);
+        });
 
-var myName = "Maartje";
+        function calculate(e)
+        {
+            var price2 = parseFloat($("#price2").val()).toFixed(3)
+            var volume2 = parseFloat($("#volume2").val())
+            var percentage2 = parseFloat($("#percentage2").val())
 
-  function sayMyName(name){
-    alert("Hi," + name + "!");
-  }
+            var result2 = price2 / (volume2 * percentage2)
 
+            $("#ppu2").val(result2)
+        }
+
+//calculations - row 3
+            $(document).ready(function(){
+                $('#volume3').keyup(calculate);
+                $('#price3').keyup(calculate);
+                $('#percentage3').keyup(calculate);
+            });
+
+            function calculate(e)
+            {
+                var price3 = parseFloat($("#price3").val()).toFixed(3)
+                var volume3 = parseFloat($("#volume3").val())
+                var percentage3 = parseFloat($("#percentage3").val())
+
+                var result3 = price3 / (volume3 * percentage3)
+
+                $("#ppu3").val(result3)
+            }
 
 
 
