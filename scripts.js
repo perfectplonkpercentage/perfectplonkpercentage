f7a93a8c57cb74ebb858d7e97d2399541cb1dc37
maartjegelsink <maartje.weenink@student.ru.nl>
2017-11-20 16:17:40 +0000

    attempt at var control

--- scripts.js
+++ scripts.js
@@ -14,9 +14,9 @@ $(document).ready(function() {
         var volume1 = parseFloat($("#volume1").val())
         var percentage1 = parseFloat($("#percentage1").val())
 
-        var result1 = price1 / (volume1 * percentage1)
+        var result1 = price1 / (volume1 * percentage1).toFixed(3)
 
-        $("#ppu1").val(result1)
+        $("#ppu1").val(result1).toFixed(3)
     }
 
 //calculations - row 2
@@ -32,9 +32,9 @@ $(document).ready(function() {
         var volume2 = parseFloat($("#volume2").val())
         var percentage2 = parseFloat($("#percentage2").val())
 
-        var result2 = price2 / (volume2 * percentage2)
+        var result2 = price2 / (volume2 * percentage2).toFixed(3)
 
-        $("#ppu2").val(result2)
+        $("#ppu2").val(result2).toFixed(3)
     }
 
 
@@ -51,9 +51,9 @@ $(document).ready(function() {
         var volume3 = parseFloat($("#volume3").val())
         var percentage3 = parseFloat($("#percentage3").val())
 
-        var result3 = price3 / (volume3 * percentage3)
+        var result3 = price3 / (volume3 * percentage3).toFixed(3)
 
-        $("#ppu3").val(result3)
+        $("#ppu3").val(result3).toFixed(3)
     }
 
 
